bd4ca543c88522abe8ea82b61b718d02c6a8da92
Allan Alvarado <allanaq08@gmail.com>
2022-02-23 17:05:54 -0600

    Archivo con el menu y comentarios con la explicación de las funciones

---
+++ Principal.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import Funciones as func
+
+
+def menu():
+    entrada_usuario = int(input('MENU. Ingrese el número de lo que '
+                                'desee hacer.\n'
+                                '1. Reconocimiento de rostro y ojos.\n'
+                                '2. Filtro pizzas.\n'
+                                '3. Filtro lazo\n'
+                                '4. Cuadro.\n'
+                                '5. Cuatro cámaras\n'
+                                '6. SALIR\n'
+                                '>>> '))
+    while True:
+
+        if entrada_usuario == 1:
+            print('Pulse "q" para volver al menu principal.\n')
+            func.reconocimiento()
+
+        elif entrada_usuario == 2:
+            print('Pulse "q" para volver al menu principal.\n')
+            func.filtros('Pizzas')
+
+        elif entrada_usuario == 3:
+            print('Pulse "q" para volver al menu principal.\n')
+            func.filtros('Lazo')
+
+        elif entrada_usuario == 4:
+            print('Pulse "q" para volver al menu principal.\n')
+            func.cuadro()
+
+        elif entrada_usuario == 5:
+            print('Pulse "q" para volver al menu principal.\n')
+            func.cuatro_camaras()
+
+        elif entrada_usuario == 6:
+            break
+
+        else:
+            print('Error: Solo ingrese un número entero del 1 al 6.\n')
+
+        entrada_usuario = int(input('MENU. Ingrese el número de lo que '
+                                    'desee hacer.\n'
+                                    '1. Reconocimiento de rostro y ojos.\n'
+                                    '2. Filtro pizzas.\n'
+                                    '3. Filtro lazo\n'
+                                    '4. Cuadro.\n'
+                                    '5. Cuatro cámaras\n'
+                                    '6. SALIR\n'
+                                    '>>> '))
+
+
+menu()
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Proyecto-Python
-Uso de opnenCV para crear filtros. 
+Uso de opnenCV para crear filtros con reconocimiento facial. 
+_____________________________________________________________
+
+Instalación de las bilbiotecas necesarias. 
+
+OpenCV:
+$ sudo apt update 
+$ sudo apt install python3-opencv
+
+Si se llegara a tener algún error con OpenCV se puede utilizar 
+el siguiente comando: 
+
+$ pip install opencv-contrib-python --upgrade
+
+
+Imutils: 
+
+$ sudo pip install imutils
+
+
